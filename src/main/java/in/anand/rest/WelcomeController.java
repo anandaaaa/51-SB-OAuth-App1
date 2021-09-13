21df5ec20466cf8b4f331e5a8c565eee69d56fa8
anand <anand980980@gmail.com>
2021-09-13 19:00:53 +0530

    initial commit

---
+++ src/main/java/in/anand/rest/WelcomeController.java
@@ -0,0 +1,15 @@
+package in.anand.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/")
+	public String welcome()
+	{
+		return "welcome to the world";
+	}
+
+}
